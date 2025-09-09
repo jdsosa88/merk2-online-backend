66a30fa21663dd34af464c9c7274564b07b842f9
jdsosa88 <jorgedeiniersosalastre88@gmail.com>
2025-09-08 21:34:15 -0500

    feat: add Swagger documentation setup for API

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -14,6 +15,20 @@ async function bootstrap() {
   });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Merk2 Online API')
+    .setDescription('API Documentation')
+    .setVersion('0.0.1')
+    //.addBearerAuth() // para JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: { persistAuthorization: true },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
