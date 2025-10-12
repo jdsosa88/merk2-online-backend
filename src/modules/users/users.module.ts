9af575a09d8f59f65da6c666cec73dfcf1b9c81f
jdsosa88 <jorgedeiniersosalastre88@gmail.com>
2025-10-12 00:00:22 -0500

    refactor: remove forwardRef import for BusinessModule in UsersModule

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -6,14 +6,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { VerificationCodeModule } from '../verification-code/verification-code.module';
 import { CaslModule } from 'src/modules/casl/casl.module';
 import { AdminUsersController } from './admin-users.controller';
-import { BusinessModule } from '../business/business.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     CaslModule,
-    VerificationCodeModule,
-    forwardRef(() => BusinessModule),
+    VerificationCodeModule,    
   ],
   controllers: [UsersController, AdminUsersController],
   providers: [UsersService],
